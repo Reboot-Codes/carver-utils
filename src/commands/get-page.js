5f33c3f36b6c0e93fc8aca33d83752c35bf16002
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-11-02 14:43:57 -0700

    get-page will now create new directories if needed

--- src/commands/get-page.js
+++ src/commands/get-page.js
@@ -5,6 +5,14 @@ import { exit } from 'process';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
+function error(spinner, message, error, options) {
+  spinner.fail(message)
+   if (options != undefined) options.debug ?
+    console.error(error) :
+    console.error(`Try rerunning with --debug (or -d) to see error messages`)
+  exit(1)
+}
+
 export default function getPage (website, outpath, options) {
   puppeteer.use(StealthPlugin())
 
@@ -18,20 +26,17 @@ export default function getPage (website, outpath, options) {
     let pageGetSpinner = ora(`Getting Page`)
     pageGetSpinner.start()
     await page.goto(website, { waitUntil: 'networkidle2' }).catch(e => {
-      pageGetSpinner.fail(`Failed to get page: ${website}`)
-      console.error(`Try rerunning with --debug (or -d) to see error messages`)
-      if (options.debug == true) console.error(e)
-      exit(1)
+      error(pageGetSpinner, `Failed to get page: ${website}`, e, options)
     })
     pageGetSpinner.succeed(`Got Page: ${website}`)
 
     let screenshotSpinner = ora(`Taking screenshot`)
     screenshotSpinner.start()
+    if (outpath != undefined && !fs.existsSync(outpath)) await fs.mkdir(outpath, { recursive: true }, e => {
+      if (e) error(screenshotSpinner, 'Failed to create an output directory', e, options);
+    })
     await page.screenshot({ path: `${outpath || "."}/screenshot.png`, fullPage: true }).catch(e => {
-      screenshotSpinner.fail(`Failed to Take Screenshot`)
-      console.error(`Try rerunning with --debug (or -d) to see error messages`)
-      if (options.debug == true) console.error(e)
-      exit(1)
+      error(screenshotSpinner, `Failed to Take Screenshot`, e, options);
     })
     screenshotSpinner.succeed(`Successfully Took a Screenshot`)
 
@@ -41,18 +46,12 @@ export default function getPage (website, outpath, options) {
       fs.writeFileSync(`${outpath || "."}/source.html`, await page.content());
       pageSourceSpinner.succeed(`Successfully Saved Source Code`)
     } catch (e) {
-      pageSourceSpinner.fail(`Failed to save HTML source`)
-      console.error(`Try rerunning with --debug (or -d) to see error messages`)
-      if (options.debug == true) console.error(e)
-      exit(1)
+      error(pageSourceSpinner, `Failed to save HTML source`, e, options)
     }
   
     if (options.silent != true) console.log(`All done, check the screenshot and source file. ✨`)
     await browser.close()
   }).catch(e => {
-    browserLaunchSpinner.fail(`Operation Failed, Error message: ${e.message}`)
-    console.error(`Try rerunning with --debug (or -d) to see error messages`)
-    if (options.debug == true) console.error(e)
-    exit(1)
+    error(browserLaunchSpinner, `${chalk.redBright('Operation Failed')}, Error message: ${e.message}`, e)
   })
 }
